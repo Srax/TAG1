abfeef403194ec6c0787422b3632dc90d0a32ae7
DD <DD@10.50.137.165>
2017-03-20 11:19:04 +0100

    kgkgkgkgkgkg

--- src/tag1/gitgut/Room.java
+++ src/tag1/gitgut/Room.java
@@ -12,5 +12,68 @@ import java.util.Scanner;
  * @author thoma
  */
 public class Room {
+    private int gold;
+    private String Discription;
+    private String North;
+    private String South;
+    private String West;
+    private String East;
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public String getDiscription() {
+        return Discription;
+    }
+
+    public void setDiscription(String Discription) {
+        this.Discription = Discription;
+    }
+
+    public String getNorth() {
+        return North;
+    }
+
+    public void setNorth(String North) {
+        this.North = North;
+    }
+
+    public String getSouth() {
+        return South;
+    }
+
+    public void setSouth(String South) {
+        this.South = South;
+    }
+
+    public String getWest() {
+        return West;
+    }
+
+    public void setWest(String West) {
+        this.West = West;
+    }
+
+    public String getEast() {
+        return East;
+    }
+
+    public void setEast(String East) {
+        this.East = East;
+    }
+
+    public Room(int gold, String Discription, String North, String South, String West, String East) {
+        this.gold = gold;
+        this.Discription = Discription;
+        this.North = North;
+        this.South = South;
+        this.West = West;
+        this.East = East;
+    }
     
 }
