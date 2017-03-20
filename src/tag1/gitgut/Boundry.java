b11933dbe68b7958236df6a0228735bcedcdaa10
thoma <thoma@10.50.137.96>
2017-03-20 13:06:21 +0100

    Added some shit to boundry

--- src/tag1/gitgut/Boundry.java
+++ src/tag1/gitgut/Boundry.java
@@ -12,10 +12,42 @@ import java.util.Scanner;
  * @author Stanislav
  */
 public class Boundry {
-    Player player = new Player("Peter");
+       
     
     public void createName() {
-        Scanner sc = new Scanner(System.in);
+    
+        boolean donePickingName = false;
+        
+        while (donePickingName == false ) {
+            System.out.println("\nWHAT IS YOUR NAME?");
+            System.out.print("NAME: ");
+            
+            Scanner sc = new Scanner(System.in);
+            
+            String inputName;         
+            
+            inputName = sc.next();      
+            Player p = new Player(inputName);
+        
+            
+            System.out.println("You chose " + "'" + p.getName() + "'" +" as your name.\n");
+            System.out.println("Hello " + p.getName() + "!");
+            System.out.println("This will be your name through the whole game, do you want to keep it?\n"
+                    + "YES/NO");
+            System.out.print("ACTION: ");
+            
+            String input;
+            input = sc.next().substring(0,1);
+            
+            if (input.equalsIgnoreCase("n")){                
+                donePickingName = false;
+            }
+            else if (input.equalsIgnoreCase("y")) {
+                System.out.println("You chose to keep your name");
+                donePickingName = true;
+            }
+            
+        }
         
     }
 }
