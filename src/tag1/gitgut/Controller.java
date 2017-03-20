3f1938069220b5482eff7100e5916055be01fada
DD <DD@10.50.137.165>
2017-03-20 11:39:06 +0100

    Roooooooms everywhere

--- src/tag1/gitgut/Controller.java
+++ src/tag1/gitgut/Controller.java
@@ -5,10 +5,41 @@
  */
 package tag1.gitgut;
 
+import java.util.Random;
+
 /**
  *
  * @author Stanislav
  */
 public class Controller {
+    Random r = new Random();
+    public void go(){
+    Room startRoom = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room1 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room2 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room3 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room4 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room5 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room6 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room7 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room8 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room9 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room10 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room11 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room12 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room13 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room14 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room15 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room16 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room17 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room18 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room19 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
+    Room room20 = new Room(r.nextInt(100)+1,"Start room, bla bla bla");
     
+        System.out.println(room8.toString());
+        System.out.println(room1.toString());
+    startRoom.setEast(room1);
+    
+    room1.setWest(startRoom);
+    }
 }
--- src/tag1/gitgut/Room.java
+++ src/tag1/gitgut/Room.java
@@ -10,13 +10,18 @@ package tag1.gitgut;
 public class Room {
     private int gold;
     private String Discription;
-    private String North;
-    private String South;
-    private String West;
-    private String East;
+    private Room North;
+    private Room South;
+    private Room West;
+    private Room East;
 
     public int getGold() {
         return gold;
+    }
+
+    public Room(int gold, String Discription) {
+        this.gold = gold;
+        this.Discription = Discription;
     }
 
     public void setGold(int gold) {
@@ -31,45 +36,43 @@ public class Room {
         this.Discription = Discription;
     }
 
-    public String getNorth() {
+    public Room getNorth() {
         return North;
     }
 
-    public void setNorth(String North) {
+    public void setNorth(Room North) {
         this.North = North;
     }
 
-    public String getSouth() {
+    public Room getSouth() {
         return South;
     }
 
-    public void setSouth(String South) {
+    public void setSouth(Room South) {
         this.South = South;
     }
 
-    public String getWest() {
+    public Room getWest() {
         return West;
     }
 
-    public void setWest(String West) {
+    public void setWest(Room West) {
         this.West = West;
     }
 
-    public String getEast() {
+    public Room getEast() {
         return East;
     }
 
-    public void setEast(String East) {
+    public void setEast(Room East) {
         this.East = East;
     }
 
-    public Room(int gold, String Discription, String North, String South, String West, String East) {
-        this.gold = gold;
-        this.Discription = Discription;
-        this.North = North;
-        this.South = South;
-        this.West = West;
-        this.East = East;
+    @Override
+    public String toString() {
+        return "Room{" + "gold=" + gold + ", Discription=" + Discription + '}';
     }
+
+    
     
 }
