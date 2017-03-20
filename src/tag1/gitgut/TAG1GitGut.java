2c91a967c8588cc55b3c77448cad345b682e3765
thoma <thoma@10.50.137.96>
2017-03-20 09:08:46 +0100

    Pull Pls

--- src/tag1/gitgut/TAG1GitGut.java
+++ src/tag1/gitgut/TAG1GitGut.java
@@ -16,6 +16,7 @@ public class TAG1GitGut {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Can i have a pull pls?");
     }
     
 }
