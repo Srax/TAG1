2ed143af4b0803cd34cd33679e02ce726ee5bc2d
thoma <thoma@10.50.137.164>
2017-03-30 14:46:11 +0200

    Added HighscoreManager.java / Score.java / ScoreComparator Added a 12 character limit to players name

--- src/App/App.java
+++ src/App/App.java
@@ -4,9 +4,8 @@ package App;
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
-
+        HighscoreManager hm = new HighscoreManager();
         Controller c = new Controller();
-        c.game();
-        
+        c.game();        
     }
 }
