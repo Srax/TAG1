674ca5bedf4a71ddbd1d3b401a3cf2fe0c1eab8d
Stanislav <Stanislav@10.50.137.53>
2017-03-31 10:23:58 +0200

    distributes items, and potions

--- src/App/Room.java
+++ src/App/Room.java
@@ -14,6 +14,7 @@ public class Room {
     private int taxCollector;
     private int trap;
     private int potion;
+    
     ArrayList<Iitem> loot = new ArrayList<>();
 
     public void add(Iitem item) {
