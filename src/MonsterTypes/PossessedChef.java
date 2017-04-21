3394b3dbe9e79227a09a2ac9fc14e7f92475d147
DD <DD@10.50.137.254>
2017-04-21 12:07:39 +0200

    Possesed chef

---
+++ src/MonsterTypes/PossessedChef.java
@@ -0,0 +1,30 @@
+package MonsterTypes;
+
+import Enteties.Player;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DD
+ */
+public class PossessedChef extends Monster {
+    
+    public PossessedChef(String name, int hp, int dmg, int armor, int gold) {
+        super(name, hp, dmg, armor, gold);
+    }
+    
+    public int monsterSpecialAttack(Player p) {
+        int damage = 0;
+
+        System.out.println("The "+getMonsterName() + "swings his laser-Meatcleaver at you, and makes a deep cut");
+        damage = this.getMonsterDmg() * 2 - p.getDef();
+
+        return damage;
+    }
+    
+}
